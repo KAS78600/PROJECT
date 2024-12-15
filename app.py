b9e6a3049dc3862f02e76488a8836b04c5741956
kas <kashfaislam759@gmail.com>
2024-12-15 21:47:23 +0530

    update

---
+++ app.py
@@ -0,0 +1,44 @@
+import pickle
+from Flask import Flask,request,jsonify,render_template
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+application= Flask(_name_)
+app=application
+
+scaler= pickle.load(open(r"D:\PROJECT\Model\StandardScaler.pkl","rb"))
+model=pickle.load(open(r"D:\PROJECT\Model\modelforprediction.pkl","rb"))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='POST':
+        temperature=float(request.form.get('temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+
+        new_data=scaler.transform([[temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        predict=model.predict(new_data)
+        
+        if predict[0]==1:
+            result= 'fire'
+        else:
+            result= 'no fire'    
+            
+
+        return render_template('home.html',result=result[0])
+    else:
+        return render_template('home.html')
+    
+    if __name__=="_main_":
+        app.run(host="0.0.0.0",debug=True)
