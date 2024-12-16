92f33875218c4eb4f993ff1a3293ad00dd6ef33b
kas <kashfaislam759@gmail.com>
2024-12-16 18:53:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pickle
-from Flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
